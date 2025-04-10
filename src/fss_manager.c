259a24aa1aca363fa51580edce5415a01da1b0b3
KimonSMN <kimonsmirlianos@gmail.com>
2025-04-10 18:57:19 +0300

    Added Flags

--- src/fss_manager.c
+++ src/fss_manager.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include <sys/inotify.h>
 #include <sys/stat.h>
@@ -10,6 +11,19 @@
 #include <time.h>
 
 int main(int argc, char* argv[]){
+
+
+    // Flags
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-l") == 0) {
+            char* manager_log = argv[++i];
+        } else if (strcmp(argv[i], "-s") == 0) {
+            char* config_file = argv[++i];
+        } else if (strcmp(argv[i], "-o") == 0){
+            int worker_limit = atoi(argv[++i]);
+        }
+    }
+
     if(mkfifo("fss_in", 0777) == -1 ){ // This file read from or wrote to by everyone
         if (errno != EEXIST){
             printf("Could not create fifo file\n");
@@ -23,7 +37,7 @@ int main(int argc, char* argv[]){
         }
     }
     printf("Opening...\n");
-    int fd = open("myfifo1", O_WRONLY);
+    int fd = open("fss_in", O_WRONLY);
     printf("Open\n");
     int x = 97;
     if(write(fd, &x, sizeof(int)) == -1){
